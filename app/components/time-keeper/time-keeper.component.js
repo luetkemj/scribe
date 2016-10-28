76dcc757381d7a015a627b6ef46fbb5976037a52
Mark Luetke <luetkemj@gmail.com>
2016-10-28 06:14:38 -0500

    time keeper component
    Fixed test container so the component actually works.
    Refactored css and added scalable variables so it can easily scale.
    Also tests for functions utils

--- app/components/time-keeper/time-keeper.component.js
+++ app/components/time-keeper/time-keeper.component.js
@@ -1,8 +1,30 @@
 import React, { PropTypes } from 'react';
 
+import { leadingZero } from '../../utils/functions';
+
 import style from './time-keeper.component.scss';
 
-export default function TimeKeeper({ day, time, sky, rotation, controls }) {
+export default function TimeKeeper({ day, time, sky, rotation, initialMs, increment }) {
+  const increment6s = () => {
+    increment(initialMs, 6000);
+  };
+
+  const increment5m = () => {
+    increment(initialMs, 300000);
+  };
+
+  const increment10m = () => {
+    increment(initialMs, 600000);
+  };
+
+  const increment1h = () => {
+    increment(initialMs, 3600000);
+  };
+
+  const increment8h = () => {
+    increment(initialMs, 28800000);
+  };
+
   const counterRotation = rotation * -1;
   const rotate = {
     transform: `rotate(${rotation}deg)`,
@@ -11,25 +33,6 @@ export default function TimeKeeper({ day, time, sky, rotation, controls }) {
     transform: `rotate(${counterRotation}deg)`,
   };
   const skyColor = style[sky];
-
-  let key = 0;
-  const controlsToRender = controls.map((control) =>
-    <button className={style.button} onClick={control.onClick} key={key++}>
-      <span className={style.duration}>{control.duration}</span>
-      <span className={style.unit}>{control.unit}</span>
-    </button>
-  );
-
-  function leadingZero(number) {
-    let newNumber;
-    if (number < 10) {
-      newNumber = `0${number}`;
-    } else {
-      newNumber = number;
-    }
-
-    return newNumber;
-  }
 
   const hours = leadingZero(time.hours);
   const minutes = leadingZero(time.minutes);
@@ -60,7 +63,26 @@ export default function TimeKeeper({ day, time, sky, rotation, controls }) {
       </div>
 
       <div className={style.controls}>
-        {controlsToRender}
+        <button className={style.button} onClick={increment6s}>
+          <span className={style.duration}>6</span>
+          <span className={style.unit}>s</span>
+        </button>
+        <button className={style.button} onClick={increment5m}>
+          <span className={style.duration}>5</span>
+          <span className={style.unit}>m</span>
+        </button>
+        <button className={style.button} onClick={increment10m}>
+          <span className={style.duration}>10</span>
+          <span className={style.unit}>m</span>
+        </button>
+        <button className={style.button} onClick={increment1h}>
+          <span className={style.duration}>1</span>
+          <span className={style.unit}>h</span>
+        </button>
+        <button className={style.button} onClick={increment8h}>
+          <span className={style.duration}>8</span>
+          <span className={style.unit}>h</span>
+        </button>
       </div>
     </div>
   );
@@ -71,5 +93,6 @@ TimeKeeper.propTypes = {
   time: PropTypes.object.isRequired,
   sky: PropTypes.string.isRequired,
   rotation: PropTypes.number.isRequired,
-  controls: PropTypes.array.isRequired,
+  increment: PropTypes.func.isRequired,
+  initialMs: PropTypes.number.isRequired,
 };
