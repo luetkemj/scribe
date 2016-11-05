a13e126a2df74ef5e90b866ea938d43df308516e
Mark Luetke <luetkemj@gmail.com>
2016-11-05 14:13:43 -0500

    Item and monster details refactor
    Both used a component that really should have been a container and a
    container that didn’t do a whole lot. So I combined the container and
    component for each.
    
    - delete item-details and monster-details components
    - migrate component css into respective containers
    - delete more form artifacts as that is so far off on the list of
    things to finish it just gets in the way for now
    - removed deleted components from test app

--- app/containers/items/item-details/item-details.container.js
+++ app/containers/items/item-details/item-details.container.js
@@ -2,9 +2,12 @@ import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import ItemDetails from '../../../components/item-details/item-details.component';
 import Spinner from '../../../components/spinner/spinner.component';
+import DefinitionList from '../../../components/definition-list/definition-list.component';
+
 import { loadItemIfNeeded } from '../../../actions/items.actions.js';
+
+import style from './item-details.container.scss';
 
 class ItemDetailsContainer extends Component {
   componentWillMount() {
@@ -23,10 +26,60 @@ class ItemDetailsContainer extends Component {
     }
 
     if (item) {
+      const detailsData = [
+        {
+          name: 'Cost',
+          stat: `${item.value} ${item.value_unit}`,
+        }, {
+          name: 'Weight',
+          stat: `${item.weight} ${item.weight_unit}`,
+        },
+        {
+          name: 'Length',
+          stat: `${item.length} ${item.length_unit}`,
+        },
+      ];
+
+      const descriptionData = [
+        {
+          name: 'description',
+          stat: item.description,
+        },
+      ];
+
+      const headerToRender = (
+        <header>
+          <h1 className={style.name}>{item.name}</h1>
+        </header>
+      );
+
+      const detailsToRender = (
+        <DefinitionList
+          definitions={detailsData}
+          dt={'name'}
+          dd={'stat'}
+        />
+      );
+
+      const descriptionToRender = (
+        <DefinitionList
+          definitions={descriptionData}
+          dt={'name'}
+          dd={'stat'}
+        />
+      );
+
       itemToRender = (
-        <ItemDetails data={item} />
+        <section className={style.itemDetails}>
+          <article>
+            {headerToRender}
+            {detailsToRender}
+            {descriptionToRender}
+          </article>
+        </section>
       );
     }
+
     return (
       <div>
         {spinnerToRender}
--- app/containers/monsters/monster-details/monster-details.container.js
+++ app/containers/monsters/monster-details/monster-details.container.js
@@ -2,9 +2,13 @@ import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import MonsterDetails from '../../../components/monster-details/monster-details.component';
 import Spinner from '../../../components/spinner/spinner.component';
+import DefinitionList from '../../../components/definition-list/definition-list.component';
+import AbilityScores from '../../../components/ability-scores/ability-scores.component';
+
 import { loadMonsterIfNeeded } from '../../../actions/monsters.actions.js';
+
+import style from './monster-details.container.scss';
 
 class MonsterDetailsContainer extends Component {
   componentWillMount() {
@@ -23,10 +27,120 @@ class MonsterDetailsContainer extends Component {
     }
 
     if (monster) {
+      let type;
+      let abilityScoresToRender;
+      let specialAbilitiesToRender;
+      let actionsToRender;
+
+      const primaryStatsData = [
+        {
+          name: 'Armor Class',
+          stat: monster.armor_class,
+        }, {
+          name: 'Hit Points',
+          stat: monster.hit_points,
+        }, {
+          name: 'Speed',
+          stat: monster.speed,
+        },
+      ];
+
+      const secondaryStatsData = [
+        {
+          name: 'Damage Resistances',
+          stat: monster.damage_resistances,
+        }, {
+          name: 'Condition Immunities',
+          stat: monster.condition_immunities,
+        }, {
+          name: 'Senses',
+          stat: monster.senses,
+        }, {
+          name: 'Languages',
+          stat: monster.languages,
+        }, {
+          name: 'Challenge',
+          stat: monster.challenge_rating,
+        },
+      ];
+
+      if (monster.subtype) {
+        type = (
+          <span>
+            {monster.type} ({monster.subtype}),
+          </span>
+        );
+      } else {
+        type = (
+          <span>
+            {monster.type},
+          </span>
+        );
+      }
+
+      const headerToRender = (
+        <header>
+          <h1 className={style.name}>{monster.name}</h1>
+          <h2 className={style.sizeTypeAlignment}>
+            {monster.size} {type} {monster.alignment}
+          </h2>
+        </header>
+      );
+
+      if (monster.ability_scores) {
+        abilityScoresToRender = (<AbilityScores ability_scores={monster.ability_scores} />);
+      }
+
+      const primaryStatsToRender = (
+        <DefinitionList
+          definitions={primaryStatsData}
+          dd={'stat'}
+          dt={'name'}
+        />
+      );
+
+      const secondaryStatsToRender = (
+        <DefinitionList
+          definitions={secondaryStatsData}
+          dd={'stat'}
+          dt={'name'}
+        />
+      );
+
+      if (monster.special_abilities) {
+        specialAbilitiesToRender = (
+          <DefinitionList
+            definitions={monster.special_abilities}
+            dd={'desc'}
+            dt={'name'}
+            verbose
+          />
+        );
+      }
+
+      if (monster.actions) {
+        actionsToRender = (
+          <DefinitionList
+            definitions={monster.actions}
+            dd={'desc'}
+            dt={'name'}
+            verbose
+          />
+        );
+      }
+
       monsterToRender = (
-        <MonsterDetails data={monster} />
+        <div className={style.monsterDetails}>
+          {headerToRender}
+          {abilityScoresToRender}
+          {primaryStatsToRender}
+          {secondaryStatsToRender}
+          {specialAbilitiesToRender}
+          {actionsToRender}
+        </div>
       );
     }
+
     return (
       <div>
         {spinnerToRender}
