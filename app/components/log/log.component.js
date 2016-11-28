fb5eb4779c0f60fa0bdea8c7a1d107ad8cb0e15a
Mark Luetke <luetkemj@gmail.com>
2016-11-28 09:19:03 -0600

    Campaign Log
    Closes #93
    
    Includes Note & Log components with test-component pages.
    
    Log Test Container with state for full CRUD in test app. Useful for
    seeing how all that the parts will fit together in the actual app.
    There is quite a bit of function pass through. Plenty of comments in
    the log test container that should help explain it.

---
+++ app/components/log/log.component.js
@@ -0,0 +1,168 @@
+import React, { Component, PropTypes } from 'react';
+import style from './log.component.scss';
+import Note from '../note/note.component';
+
+export default class Log extends Component {
+  state = {
+    editing: false,
+    addingNote: false,
+  }
+
+  edit = (event) => {
+    this.setState({
+      editing: true,
+    });
+
+    event.currentTarget.blur();
+    event.preventDefault();
+
+    this.props.editNotes();
+  }
+
+  save = (event, notes) => {
+    this.setState({
+      editing: false,
+    });
+
+    event.currentTarget.blur();
+    event.preventDefault();
+
+    this.props.save(notes);
+  }
+
+  cancel = (event) => {
+    this.setState({
+      editing: false,
+      addingNote: false,
+    });
+
+    event.currentTarget.blur();
+    event.preventDefault();
+
+    this.props.cancelEditNotes();
+  }
+
+  addNote = (event) => {
+    this.setState({
+      addingNote: true,
+    });
+
+    event.currentTarget.blur();
+    event.preventDefault();
+
+    this.props.addNote();
+  }
+
+  cancelAddNote = (event) => {
+    this.setState({
+      addingNote: false,
+    });
+
+    event.currentTarget.blur();
+    event.preventDefault();
+
+    this.props.cancelAddNote();
+  }
+
+  submitAddNote = (event, id, content) => {
+    this.setState({
+      addingNote: false,
+    });
+
+    event.currentTarget.blur();
+    event.preventDefault();
+
+    this.props.submitAddNote(id, content);
+  }
+
+  updateTempNotes = (id, content) => {
+    this.props.updateTempNotes(id, content);
+  }
+
+  render() {
+    const { day, time, weather, season, notes } = this.props;
+
+    let controlsToRender;
+    let addNoteButton;
+    let notesToRender;
+
+    if (this.state.editing) {
+      controlsToRender = (
+        <div className={style.controls}>
+          <button className={style.cancel} onClick={this.cancel}>CANCEL</button>
+          <button className={style.save} onClick={this.save}>SAVE</button>
+        </div>
+      );
+    } else {
+      controlsToRender = (
+        <div className={style.controls}>
+          <button className={style.edit} onClick={this.edit}>EDIT</button>
+        </div>
+      );
+    }
+
+    if (!this.state.addingNote && !this.state.editing) {
+      addNoteButton = (
+        <button
+          className={style.addNote}
+          onClick={this.addNote}
+        >
+          ADD NOTE
+        </button>
+      );
+    }
+
+    if (notes) {
+      notesToRender = notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index.toString()}
+          heading={note.heading}
+          content={note.content}
+          creating={note.creating}
+          deleting={note.deleting}
+          editing={this.state.editing}
+          submitAddNote={this.submitAddNote}
+          cancelAddNote={this.cancelAddNote}
+          updateTempNotes={this.updateTempNotes}
+          toggleNoteDeletion={this.props.toggleNoteDeletion}
+        />
+      ));
+    }
+
+    return (
+      <div className={style.log}>
+        {controlsToRender}
+        <div className={style.logHeading}>
+          <div>
+            <div className={style.day}>Day {day}</div>
+            <div className={style.time}>{time}</div>
+          </div>
+          <div>
+            <div className={style.season}>{season}</div>
+            <div className={style.temp}>{weather}</div>
+          </div>
+        </div>
+        {notesToRender}
+        {addNoteButton}
+      </div>
+    );
+  }
+}
+
+Log.propTypes = {
+  save: PropTypes.func.isRequired,
+  addNote: PropTypes.func.isRequired,
+  cancelAddNote: PropTypes.func.isRequired,
+  submitAddNote: PropTypes.func.isRequired,
+  editNotes: PropTypes.func.isRequired,
+  cancelEditNotes: PropTypes.func.isRequired,
+  updateTempNotes: PropTypes.func.isRequired,
+  toggleNoteDeletion: PropTypes.func.isRequired,
+
+  day: PropTypes.number.isRequired,
+  time: PropTypes.string.isRequired,
+  season: PropTypes.string.isRequired,
+  weather: PropTypes.string.isRequired,
+  notes: PropTypes.array,
+};
