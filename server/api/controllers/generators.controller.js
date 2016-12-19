e1727128fde88d3773a09fcb08b4559d355e464f
Mark Luetke <luetkemj@gmail.com>
2016-12-19 08:24:03 -0600

    WIP

--- server/api/controllers/generators.controller.js
+++ server/api/controllers/generators.controller.js
@@ -1,14 +1,24 @@
 import mongoose from 'mongoose';
 import * as _ from 'lodash';
-import { shimmy, getRandomInt, d30, generateStorm } from '../../lib/generators';
+import {
+  shimmy,
+  getRandomInt,
+  d30,
+  generateStorm,
+  assignTime,
+  trackStorm } from '../../lib/generators';
 import { ZONE_VARIANCE, STORM_TYPE_TABLE } from '../../config/constants/weather.constants';
 
 const logger = require('../../lib/logger')();
 
 const Generator = mongoose.model('Generator');
 
+/*
+ * @TODO: there is a lot happening here... can it be simplified further? tested at all?
+ * cause at the moment it is buggy!
+ */
 export function generateWeather(req, res) {
-  const { zone, terrain, season, month } = req.body;
+  const { zone, terrain, season, month, initialMs } = req.body;
 
   Generator
   .find({ name: 'weather' })
@@ -31,16 +41,31 @@ export function generateWeather(req, res) {
       const temps = [];
       // create an array of temps within our range of high and low
       for (let i = 0; i < 24; i += 1) {
-        temps.push(getRandomInt(low, high));
+        temps.push({ temp: getRandomInt(low, high) });
       }
 
       let stormType = 'none';
       if (weatherClass) {
-        stormType = STORM_TYPE_TABLE[weatherClass][d30()];
+        const roll = d30();
+        logger.log('stormType: weatherClass: %s', weatherClass);
+        logger.log('stormType: roll: %s', roll);
+        stormType = STORM_TYPE_TABLE[weatherClass][roll];
+        logger.log('stormType: %s', stormType);
       }
 
       // reverse order temps and shimmy into an ordered set of observed temps by hour of the day
-      const observed = shimmy(_.chain(temps).orderBy().reverse().value());
+      const observedTempsByHour = assignTime(
+        shimmy(_.chain(temps).orderBy('temp').reverse().value()),
+        initialMs
+      );
+
+      // if storm track and map to observedTempsByHour
+      // otherwise just return observedTempsByHour
+      let observed = observedTempsByHour;
+      logger.log('stormType: %s', stormType);
+      if (stormType !== 'none') {
+        observed = trackStorm(observedTempsByHour, generateStorm(stormType));
+      }
 
       const currentWeather = {
         temp: {
@@ -48,7 +73,6 @@ export function generateWeather(req, res) {
             low,
             high,
             stormType,
-            storm: generateStorm(stormType),
           },
           observed,
         },
--- server/lib/generators.js
+++ server/lib/generators.js
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 import { NS_CELL_TABLE, S_CELL_TABLE } from '../config/constants/weather.constants';
 
 const logger = require('./logger')();
@@ -7,15 +8,20 @@ export function getRandomInt(min, max) {
 }
 
 export function d6() {
-  return getRandomInt(0, 6);
+  return getRandomInt(1, 6);
 }
 
 export function d10() {
-  return getRandomInt(0, 10);
+  return getRandomInt(1, 10);
 }
 
 export function d30() {
-  return getRandomInt(0, 30);
+  return getRandomInt(1, 30);
+}
+
+// convert minutes to milliseconds
+export function toMs(min) {
+  return min * 60000;
 }
 
 // alternates pushing and shifting elements from candidate onto new array.
@@ -34,30 +40,101 @@ export function shimmy(array) {
   return newArray;
 }
 
-export function generateSingleCell() {
-  return [{
-    duration: d10() + 20,
-    effect: NS_CELL_TABLE[d30()],
-  }];
+/*
+ * @TODO: test this bugger!
+ */
+export function assignTime(array, initialMs) {
+  return array.map((item, index) => ({
+    ...item,
+    // add one hour per item in array. This sets up our initial hourly observed forcast.
+    time: initialMs + (3600000 * index),
+  }));
 }
 
+/*
+ * @TODO: test this bugger!
+ * will need to break it into smaller bits and get the random func out of there.
+ */
+export function trackStorm(hourly, storm) {
+  logger.log('trackStorm: storm: %j', storm);
+  const startOfDay = hourly[0].time;
+  // get endofday
+  const endOfDay = startOfDay + 86400000;
+  // subtract storm duration
+  const endOfStormWindow = endOfDay - storm.duration;
+  // get random time from start of day to last possible start of storm
+  const stormStartTime = getRandomInt(startOfDay, endOfStormWindow);
+  let time = stormStartTime;
+  logger.log('trackStorm: time: %s', time);
+  logger.log('trackStorm: stormStartTime: %s', stormStartTime);
+
+  for (let i = 0; i < storm.cells.length; i += 1) {
+    hourly.push({
+      time,
+      cell: storm.cells[i],
+    });
+
+    time += storm.cells[i].duration + storm.cells[i].delay;
+    logger.log('trackStorm: time: %s', time);
+  }
+
+  const newArray = _.orderBy(hourly, 'time');
+
+  for (let i = 0; i < newArray.length; i += 1) {
+    if (_.isUndefined(newArray[i].temp)) {
+      newArray[i].temp = newArray[i - 1].temp;
+    }
+  }
+
+  return newArray;
+}
+
+/*
+ * @TODO: test this bugger!
+ */
+export function generateSingleCell() {
+  const duration = toMs(d10() + 20);
+  return {
+    duration,
+    cells: [{
+      duration,
+      effect: NS_CELL_TABLE[d30()],
+    }],
+  };
+}
+
+/*
+ * @TODO: test this bugger!
+ */
 export function generateMultiCell(table, cluster) {
   const cells = [];
+  let totalDuration = 0;
+
   for (let i = 0; i < d6() + 2; i += 1) {
     let delay;
     if (cluster) {
       delay = Math.ceil(d30() / 2);
+      totalDuration += toMs(delay);
     }
 
+    const duration = toMs(d10() + 20);
+    totalDuration += duration;
+
     cells.push({
-      duration: d10() + 20,
+      duration,
       effect: table[d30()],
-      delay,
+      delay: toMs(delay),
     });
   }
-  return cells;
+  return {
+    duration: totalDuration,
+    cells,
+  };
 }
 
+/*
+ * @TODO: test this bugger!
+ */
 export function generateSuperCell() {
   const duration = 60 + (d30() * 10);
   const cells = [];
@@ -65,55 +142,64 @@ export function generateSuperCell() {
   for (let i = 1; i <= duration / 10; i += 1) {
     if (i === 1) {
       cells.push({
-        duration: 10,
+        duration: toMs(10),
         effect: S_CELL_TABLE[d10()],
       });
     }
 
     if (i === 2) {
       cells.push({
-        duration: 10,
+        duration: toMs(10),
         effect: S_CELL_TABLE[d10() + 10],
       });
     }
 
     if (i === duration) {
       cells.push({
-        duration: 10,
+        duration: toMs(10),
         effect: S_CELL_TABLE[d10()],
       });
     }
 
     if (i > 2 && i < duration) {
       cells.push({
-        duration: 10,
+        duration: toMs(10),
         effect: S_CELL_TABLE[d10() + 20],
       });
     }
   }
 
-  return cells;
+  return {
+    duration: toMs(duration),
+    cells,
+  };
 }
 
 
+/*
+ * @TODO: test this bugger!
+ */
 export function generateStorm(stormType) {
+  let storm;
   if (stormType === 'singleCell') {
-    return generateSingleCell();
+    storm = generateSingleCell();
   }
   if (stormType === 'multiCellClusterNS') {
-    return generateMultiCell(NS_CELL_TABLE, true);
+    storm = generateMultiCell(NS_CELL_TABLE, true);
   }
   if (stormType === 'multiCellClusterS') {
-    return generateMultiCell(S_CELL_TABLE, true);
+    storm = generateMultiCell(S_CELL_TABLE, true);
   }
   if (stormType === 'multiCellLineNS') {
-    return generateMultiCell(NS_CELL_TABLE, false);
+    storm = generateMultiCell(NS_CELL_TABLE, false);
   }
   if (stormType === 'multiCellLineS') {
-    return generateMultiCell(S_CELL_TABLE, false);
+    storm = generateMultiCell(S_CELL_TABLE, false);
   }
   if (stormType === 'superCell') {
-    return generateSuperCell();
+    storm = generateSuperCell();
   }
-  return null;
+  return {
+    ...storm,
+  };
 }
