ce314394da9e95662092629fdfdb8d44ddd4e3bd
Mark Luetke <luetkemj@gmail.com>
2017-01-08 09:07:57 -0600

    fix spinning sundial
    sundial spun super fast from 0 to current time. This fixes that by
    adding a hasMounted class to timekeeper 300ms after componentWillMount.
    It is this class that we added the spinning transitions. So basically
    we just wait for everything to get into place before allowing
    transitions

--- app/components/time-keeper/time-keeper.component.jsx
+++ app/components/time-keeper/time-keeper.component.jsx
@@ -1,92 +1,112 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 
 import { leadingZero } from '../../utils/functions';
 
 import style from './time-keeper.component.scss';
 
-export default function TimeKeeper(
-  { day, time, sky, rotation, initialMs, increment, phaseOfMoon }) {
-  const increment6s = () => {
-    increment(initialMs, 6000);
+export default class TimeKeeper extends Component {
+  state = {
+    mounted: false,
+  }
+
+  componentWillMount() {
+    setTimeout(() => {
+      this.setState({
+        mounted: true,
+      });
+    }, 300);
+  }
+
+
+  increment6s = () => {
+    this.props.increment(this.props.initialMs, 6000);
   };
 
-  const increment5m = () => {
-    increment(initialMs, 300000);
+  increment5m = () => {
+    this.props.increment(this.props.initialMs, 300000);
   };
 
-  const increment10m = () => {
-    increment(initialMs, 600000);
+  increment10m = () => {
+    this.props.increment(this.props.initialMs, 600000);
   };
 
-  const increment1h = () => {
-    increment(initialMs, 3600000);
+  increment1h = () => {
+    this.props.increment(this.props.initialMs, 3600000);
   };
 
-  const increment8h = () => {
-    increment(initialMs, 28800000);
+  increment8h = () => {
+    this.props.increment(this.props.initialMs, 28800000);
   };
 
-  const counterRotation = rotation * -1;
-  const rotate = {
-    transform: `rotate(${rotation}deg)`,
-  };
-  const counterRotate = {
-    transform: `rotate(${counterRotation}deg)`,
-  };
-  const skyColor = style[sky];
+  render() {
+    let timeKeeperClasses = style.timeKeeper;
+    if (this.state.mounted) {
+      timeKeeperClasses += ` ${style.hasMounted}`;
+    }
 
-  const hours = leadingZero(time.hours);
-  const minutes = leadingZero(time.minutes);
-  const seconds = leadingZero(time.seconds);
+    const { day, time, sky, rotation, phaseOfMoon } = this.props;
+    const counterRotation = rotation * -1;
+    const rotate = {
+      transform: `rotate(${rotation}deg)`,
+    };
+    const counterRotate = {
+      transform: `rotate(${counterRotation}deg)`,
+    };
+    const skyColor = style[sky];
 
-  const phaseClass = `moon${phaseOfMoon}`;
-  const moonClasses = `${style.moon} ${style[phaseClass]}`;
+    const hours = leadingZero(time.hours);
+    const minutes = leadingZero(time.minutes);
+    const seconds = leadingZero(time.seconds);
 
-  return (
-    <div className={style.timeKeeper}>
-      <div className={style.dayCount}>DAY {day}</div>
-      <div className={style.sundial}>
-        <div className={`${style.sky} ${skyColor}`}>
-          <div className={style.stars} />
-          <div className={style.sunMoon} style={rotate}>
-            <div className={style.sun} style={counterRotate} />
-            <div className={moonClasses} style={counterRotate} />
+    const phaseClass = `moon${phaseOfMoon}`;
+    const moonClasses = `${style.moon} ${style[phaseClass]}`;
+
+    return (
+      <div className={timeKeeperClasses}>
+        <div className={style.dayCount}>DAY {day}</div>
+        <div className={style.sundial}>
+          <div className={`${style.sky} ${skyColor}`}>
+            <div className={style.stars} />
+            <div className={style.sunMoon} style={rotate}>
+              <div className={style.sun} style={counterRotate} />
+              <div className={moonClasses} style={counterRotate} />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={style.time}>
-        {hours}
-        <span className={style.colon}>:</span>
-        {minutes}
-        <span className={style.colon}>:</span>
-        {seconds}
-      </div>
+        <div className={style.time}>
+          {hours}
+          <span className={style.colon}>:</span>
+          {minutes}
+          <span className={style.colon}>:</span>
+          {seconds}
+        </div>
 
-      <div className={style.controls}>
-        <button className={style.button} onClick={increment6s}>
-          <span className={style.duration}>6</span>
-          <span className={style.unit}>s</span>
-        </button>
-        <button className={style.button} onClick={increment5m}>
-          <span className={style.duration}>5</span>
-          <span className={style.unit}>m</span>
-        </button>
-        <button className={style.button} onClick={increment10m}>
-          <span className={style.duration}>10</span>
-          <span className={style.unit}>m</span>
-        </button>
-        <button className={style.button} onClick={increment1h}>
-          <span className={style.duration}>1</span>
-          <span className={style.unit}>h</span>
-        </button>
-        <button className={style.button} onClick={increment8h}>
-          <span className={style.duration}>8</span>
-          <span className={style.unit}>h</span>
-        </button>
+        <div className={style.controls}>
+          <button className={style.button} onClick={this.increment6s}>
+            <span className={style.duration}>6</span>
+            <span className={style.unit}>s</span>
+          </button>
+          <button className={style.button} onClick={this.increment5m}>
+            <span className={style.duration}>5</span>
+            <span className={style.unit}>m</span>
+          </button>
+          <button className={style.button} onClick={this.increment10m}>
+            <span className={style.duration}>10</span>
+            <span className={style.unit}>m</span>
+          </button>
+          <button className={style.button} onClick={this.increment1h}>
+            <span className={style.duration}>1</span>
+            <span className={style.unit}>h</span>
+          </button>
+          <button className={style.button} onClick={this.increment8h}>
+            <span className={style.duration}>8</span>
+            <span className={style.unit}>h</span>
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 TimeKeeper.propTypes = {
