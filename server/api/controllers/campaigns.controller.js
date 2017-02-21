7ba190c3f587375a79a061e5508d7deff57bf075
Mark Luetke <luetkemj@gmail.com>
2017-02-21 07:59:25 -0600

    Fix API
    no need to check req.user in API because we do that in express on every
    request. Instead we run all our queries with the req.user to make sure
    we only get results the current user owns.

--- server/api/controllers/campaigns.controller.js
+++ server/api/controllers/campaigns.controller.js
@@ -5,99 +5,90 @@ const logger = require('../../lib/logger')();
 const Campaign = mongoose.model('Campaign');
 
 export function getCampaigns(req, res) {
-  if (req.user) {
-    Campaign
-    .find({
-      userId: req.user.id,
-    })
-    .sort('name')
-    .lean()
-    .exec((err, campaigns) => {
-      if (!err) {
-        logger.log(`getCampaigns: campaigns.length: ${campaigns.length}`);
-        return res.send(campaigns);
-      }
-      logger.log('getCampaigns Error: %j', err);
-      return res.send(err);
-    });
-  }
-  return res.status(401).send({ error: 'Unauthorized' });
+  Campaign
+  .find({
+    userId: req.user.id,
+  })
+  .sort('name')
+  .lean()
+  .exec((err, campaigns) => {
+    if (!err) {
+      logger.log(`getCampaigns: campaigns.length: ${campaigns.length}`);
+      return res.send(campaigns);
+    }
+    logger.log('getCampaigns Error: %j', err);
+    return res.send(err);
+  });
 }
 
 export function getCampaign(req, res) {
-  if (req.user) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    Campaign
-    .find({
-      _id: id,
-      userId: req.user.id,
-    })
-    .lean()
-    .exec((err, campaign) => {
-      if (!err) {
-        logger.log('getCampaign: %o', campaign);
-        return res.send(campaign);
-      }
-      logger.log('getCampaign Error: %j', err);
-      return res.send(err);
-    });
-  }
-  return res.status(401).send({ error: 'Unauthorized' });
+  Campaign
+  .find({
+    _id: id,
+    userId: req.user.id,
+  })
+  .lean()
+  .exec((err, campaign) => {
+    if (!err) {
+      logger.log('getCampaign: %o', campaign);
+      return res.send(campaign);
+    }
+    logger.log('getCampaign Error: %j', err);
+    return res.send(err);
+  });
 }
 
 export function createCampaign(req, res) {
-  if (req.user) {
-    const campaign = {
-      name: req.body.name,
-      userId: req.user.id,
-    };
+  const campaign = {
+    name: req.body.name,
+    userId: req.user.id,
+  };
 
-    logger.log('createCampaign: %j', campaign);
+  logger.log('createCampaign: %j', campaign);
 
-    Campaign.create(req.body, (err, createdCampaign) => {
-      if (err) {
-        logger.log(`Error: ${err}`);
-        return res.send(err);
-      }
+  Campaign.create(campaign, (err, createdCampaign) => {
+    if (err) {
+      logger.log(`Error: ${err}`);
+      return res.send(err);
+    }
 
-      logger.log('createCampaign: %o', createdCampaign);
-      return res.send(createdCampaign);
-    });
-  }
-  return res.status(401).send({ error: 'Unauthorized' });
+    logger.log('createCampaign: %o', createdCampaign);
+    return res.send(createdCampaign);
+  });
 }
 
 export function updateCampaign(req, res) {
-  if (req.user) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    Campaign.findByIdAndUpdate(id, { $set: req.body }, (err, campaign) => {
-      if (err) {
-        logger.log(`Error: ${err}`);
-        return res.send(err);
-      }
+  Campaign.update({
+    _id: id,
+    userId: req.user.id,
+  }, { $set: req.body }, (err, campaign) => {
+    if (err) {
+      logger.log(`Error: ${err}`);
+      return res.send(err);
+    }
 
-      logger.log('updateCampaign: %o', campaign);
-      return res.send(campaign);
-    });
-  }
-  return res.status(401).send({ error: 'Unauthorized' });
+    logger.log('updateCampaign: %o', campaign);
+    return res.send(campaign);
+  });
 }
 
 export function deleteCampaign(req, res) {
-  if (req.user) {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    Campaign.findByIdAndRemove(id, {}, (err, campaign) => {
-      if (err) {
-        logger.log(`Error: ${err}`);
-        return res.send(err);
-      }
+  Campaign.remove({
+    _id: id,
+    userId: req.user.id,
+  }, (err, campaign) => {
+    if (err) {
+      logger.log(`Error: ${err}`);
+      return res.send(err);
+    }
 
-      logger.log('deleteCampaign: %o', campaign);
-      return res.send(campaign);
-    });
-  }
-  return res.status(401).send({ error: 'Unauthorized' });
+    logger.log('deleteCampaign: %o', campaign);
+    return res.send(campaign);
+  });
 }
