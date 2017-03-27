bf215035105bbd7e85851e19f5a8d74ca7ca40d5
Mark Luetke <luetkemj@gmail.com>
2017-03-27 10:05:59 -0500

    remove unused monster routes
    create update and delete are not being used. Delete them all.

--- server/api/controllers/monsters.controller.js
+++ server/api/controllers/monsters.controller.js
@@ -6,20 +6,6 @@ const logger = require('../../lib/logger')();
 
 const Monster = mongoose.model('Monster');
 
-export function createMonster(req, res) {
-  logger.log('createMonster: %o', req.body);
-
-  Monster.create(req.body, (err, monster) => {
-    if (err) {
-      logger.log(`Error: ${err}`);
-      return res.send(err);
-    }
-
-    logger.log('createMonster: %o', monster);
-    return res.send(monster);
-  });
-}
-
 export function getMonsters(req, res) {
   const { limit, skip } = req.query;
 
@@ -54,33 +40,5 @@ export function getMonster(req, res) {
     }
     logger.log('getMonster Error: %j', err);
     return res.send(err);
-  });
-}
-
-export function updateMonster(req, res) {
-  const { id } = req.params;
-
-  Monster.findByIdAndUpdate(id, { $set: req.body }, (err, monster) => {
-    if (err) {
-      logger.log(`Error: ${err}`);
-      return res.send(err);
-    }
-
-    logger.log('updateMonster: %o', monster);
-    return res.send(monster);
-  });
-}
-
-export function deleteMonster(req, res) {
-  const { id } = req.params;
-
-  Monster.findByIdAndRemove(id, {}, (err, monster) => {
-    if (err) {
-      logger.log(`Error: ${err}`);
-      return res.send(err);
-    }
-
-    logger.log('deleteMonster: %o', monster);
-    return res.send(monster);
   });
 }
--- server/api/routes/monsters.route.js
+++ server/api/routes/monsters.route.js
@@ -1,15 +1,9 @@
 import {
-  deleteMonster,
   getMonster,
   getMonsters,
-  createMonster,
-  updateMonster,
 } from '../controllers/monsters.controller';
 
 module.exports = (router) => {
   router.route('/api/monsters').get(getMonsters);
   router.route('/api/monsters/:id').get(getMonster);
-  router.route('/api/monsters').post(createMonster);
-  router.route('/api/monsters/:id').patch(updateMonster);
-  router.route('/api/monsters/:id').delete(deleteMonster);
 };
