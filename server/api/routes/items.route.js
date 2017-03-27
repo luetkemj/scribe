2bda26de1e03c0bf4dee0e640ed63b4c9b58bd03
Mark Luetke <luetkemj@gmail.com>
2017-03-27 09:19:18 -0500

    Remove unused items routes
    create update and delete all unused.

--- server/api/routes/items.route.js
+++ server/api/routes/items.route.js
@@ -1,15 +1,9 @@
 import {
-  deleteItem,
   getItem,
   getItems,
-  createItem,
-  updateItem,
 } from '../controllers/items.controller';
 
 module.exports = (router) => {
   router.route('/api/items').get(getItems);
   router.route('/api/items/:id').get(getItem);
-  router.route('/api/items').post(createItem);
-  router.route('/api/items/:id').patch(updateItem);
-  router.route('/api/items/:id').delete(deleteItem);
 };
